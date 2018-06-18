885b5e00a395b3926c7598df63931a875ea11f62
Dirk Schaap <dwgs76@gmail.com>
2018-06-18 13:51:21 +0200

    fresh start

---
+++ src/main/java/com/robincarels/tattooapp/TattooappApplication.java
@@ -0,0 +1,12 @@
+package com.robincarels.tattooapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TattooappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TattooappApplication.class, args);
+	}
+}
